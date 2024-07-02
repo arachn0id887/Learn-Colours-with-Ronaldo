ddc7bd48231ac0e4e03da1b75342701c98863957
arachn0id887 <vivaanjha@gmail.com>
2024-07-02 17:34:18 +0530

    Vercel Analytics Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Ronaldo from "./Ronaldo"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
       <Ronaldo/>
+     <Analytics/>
     </>
   )
 }
